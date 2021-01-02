cbeab1475b5ca69c46e293d9397581d661dbfa9d
Dorikan <ci06122004@gmail.com>
2021-01-02 03:25:56 +0300

    выполнено задание номер 2.

--- app/src/main/java/com/example/geekbrainsa1l1firstapp/MainActivity.java
+++ app/src/main/java/com/example/geekbrainsa1l1firstapp/MainActivity.java
@@ -1,35 +1,37 @@
+
 package com.example.geekbrainsa1l1firstapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editText;
-    TextView textView;
+    TextView textViewCity;
     Button btnOk;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        btnOkSetOnClickListener();
+    }
 
-        editText = (EditText) findViewById(R.id.textInputEditText1);
-        textView = (TextView) findViewById(R.id.textView2);
-        btnOk = (Button) findViewById(R.id.buttonInputCity);
-        View.OnClickListener oclBtnOk = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText(editText.getText().toString());
-            }
-        };
+    private void btnOkSetOnClickListener() {
+        View.OnClickListener oclBtnOk = v -> textViewCity.setText(editText.getText().toString());
         btnOk.setOnClickListener(oclBtnOk);
+    }
+
+    private void findViews() {
+        editText = findViewById(R.id.textInputCity);
+        textViewCity = findViewById(R.id.usersCityFromEditText);
+        btnOk = findViewById(R.id.buttonInputCity);
     }
 }
